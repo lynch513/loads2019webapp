0419e8b48622762a502dbfe11c3658db039d38cd
lynch513 <lynch513@yandex.ru>
2019-08-05 22:14:37 +0300

    Test Fable.Browser.Dom working

--- src/App.fs
+++ src/App.fs
@@ -1,3 +1,26 @@
 module App
 
-printfn "Hello from Fable"
+open Browser.Dom
+
+let minus = document.getElementById "minus"
+let plus = document.getElementById "plus"
+
+let mutable count = 0
+
+let textcount = document.getElementById "textcount"
+textcount.innerHTML <- (string count)
+
+minus.addEventListener(
+    "click",
+    fun _ -> 
+        count <- count - 1
+        textcount.innerHTML <- (string count)
+)
+
+plus.addEventListener(
+    "click",
+    fun _ -> 
+        count <- count + 1
+        textcount.innerHTML <- (string count)
+)
+
